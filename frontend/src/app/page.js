29e941cf71236c9fae983e65e853d4c15a917613
jinhyeok <skan9708@naver.com>
2025-03-28 22:17:26 +0900

    회원 등록 표 수정

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import CardContainer from "@/components/CardContainer";
+import CardContainer from "@/app/components/CardContainer";
 
 export default function Home() {
   return (
