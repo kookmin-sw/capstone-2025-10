fc92c0d1cad43a29a8a744fdde78d81cd926f367
millook <kkmj1374@gmail.com>
2025-04-03 00:45:35 +0900

    [feat] product controller implementation #35

--- backend/src/main/java/capstone/offflow/Dashboard/Controller/ProductController.java
+++ backend/src/main/java/capstone/offflow/Dashboard/Controller/ProductController.java
@@ -4,6 +4,7 @@ package capstone.offflow.Dashboard.Controller;
 import capstone.offflow.Dashboard.Dto.ProductDto;
 import capstone.offflow.Dashboard.Repository.ProductRepository;
 import capstone.offflow.Dashboard.Service.ProductService;
+import capstone.offflow.User.Domain.User;
 import capstone.offflow.User.Service.UserPrincipal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -29,47 +30,49 @@ public class ProductController {
 
 
     //상품 생성
-    // Valid + BindingResult => DTO 필드 유효성 검사 (Notblank, notnull, size등)
+    // 별도 예외처리 필요없음 -> 예외 핸들러가 예외발생시 중간 개입후 처리
     @PostMapping("/create")
     public ResponseEntity<?> createProduct(
             @RequestBody @Validated ProductDto productDto,
-            BindingResult result,
             @AuthenticationPrincipal UserPrincipal userPrincipal
-            ){
-        if (result.hasErrors()){
-            Map<String, String> errors = new HashMap<>();
-            result.getFieldErrors().forEach(error -> errors.put(error.getField(),
-                    error.getDefaultMessage()));
-            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-        }
-
-        try{
-            productService.createProduct(productDto, userPrincipal.getUser());
-            return new ResponseEntity<>("Product created Successfully", HttpStatus.CREATED);
-        } catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    ) {
+        productService.createProduct(productDto, userPrincipal.getUser());
+        return ResponseEntity.status(HttpStatus.CREATED).body("Product created Successfully");
     }
 
     //상품 조회
     @GetMapping("/{id}")
-    public ResponseEntity<?> getProduct(){
-        return null;
+    public ResponseEntity<?> getProduct(
+            @PathVariable(name = "id") Long id,
+            @AuthenticationPrincipal UserPrincipal userPrincipal) {
+
+        ProductDto dto = productService.getProductById(id, userPrincipal.getUser());
+        return ResponseEntity.ok(dto);
+
     }
 
 
     //상품 수정
     @PatchMapping("/{id}")
-    public ResponseEntity<?> updateProduct(){
-        return null;
+    public ResponseEntity<?> updateProduct(
+            @PathVariable(name = "id") Long id,
+            @RequestBody ProductDto dto,
+            @AuthenticationPrincipal UserPrincipal userPrincipal){
+
+        productService.updateProduct(dto, userPrincipal.getUser());
+        return ResponseEntity.ok("Product updated successfully");
     }
 
 
 
     //상품 삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProduct(){
-        return null;
+    public ResponseEntity<?> deleteProduct(
+            @PathVariable(name = "id") Long id,
+            @AuthenticationPrincipal UserPrincipal userPrincipal){
+
+        productService.deleteProduct(id, userPrincipal.getUser());
+        return ResponseEntity.ok("Product delete successfully");
     }
 
 }
