ca06742fa5dd779fb37996100a3d0fc823eaaf46
millook <kkmj1374@gmail.com>
2025-04-13 01:20:11 +0900

    [feat] dashboard Dto & imageUrl add #81

--- backend/src/main/java/capstone/offflow/Dashboard/Dto/DashboardDto.java
+++ backend/src/main/java/capstone/offflow/Dashboard/Dto/DashboardDto.java
@@ -1,7 +1,10 @@
 package capstone.offflow.Dashboard.Dto;
 
 import capstone.offflow.Dashboard.Domain.Dashboard;
+import capstone.offflow.Dashboard.Domain.Product;
 import lombok.*;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +19,7 @@ public class DashboardDto {
     private Long id;
     private String dashboardName;
 
+    private String imageUrl;
     private Date startDate;
     private Date endDate;
 
@@ -29,6 +33,7 @@ public class DashboardDto {
         return DashboardDto.builder()
                 .id(dashboard.getId())
                 .dashboardName(dashboard.getDashboardName())
+                .imageUrl(dashboard.getImageUrl())
                 .startDate(dashboard.getStartDate())
                 .endDate(dashboard.getEndDate())
                 .metadata(MetadataDto.convertToDto(dashboard.getMetadata()))
@@ -36,6 +41,19 @@ public class DashboardDto {
                         .map(SectionDto::convertToDto)
                         .collect(Collectors.toList()))
                 .build();
+    }
+
+
+    //DTO -> Entity (생성/수정용)
+    public static Dashboard convertToEntity(DashboardDto dashboardDto){
+        Dashboard dashboard = new Dashboard();
+        dashboard.setDashboardName(dashboardDto.getDashboardName());
+        dashboard.setImageUrl(dashboardDto.getImageUrl());
+        dashboard.setStartDate(dashboardDto.getStartDate());
+        dashboard.setEndDate(dashboardDto.getEndDate());
+        dashboard.setMetadata(MetadataDto.convertToEntity(dashboardDto.getMetadata())); // ✅ 변환 필요
+        dashboard.setSections(new ArrayList<>());
+        return dashboard;
     }
 
 }
